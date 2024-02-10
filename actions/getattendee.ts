170513e6532e57c3fa2393a86edfda32148d6e2f
Sushil Gupta <kalwarsushil2@gmail.com>
2024-02-10 13:50:03 +0530

    getAttendee refactor

--- actions/getattendee.ts
+++ actions/getattendee.ts
@@ -1,25 +1,28 @@
-import { NextResponse } from "next/server";
+import { prismaClient } from "@/lib/prismaClient";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { prismaClient } from "@/lib/prismaClient";
-
 export const getAttendee = async (attendeeId: string) => {
-  const session = await getServerSession(authOptions);
+  try {
+    const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return "not authorized";
+    if (!session) {
+      return "not authorized";
+    }
+
+    const attendee = await prismaClient.attendee.findUnique({
+      where: {
+        id: attendeeId,
+      },
+    });
+
+    if (!attendee) {
+      return null;
+    }
+
+    return attendee;
+  } catch (error) {
+    console.log("GETATTENDEE_ACTIONS");
+    console.log(error);
   }
-
-  const attendee = await prismaClient.attendee.findUnique({
-    where: {
-      id: attendeeId,
-    },
-  });
-
-  if (!attendee) {
-    return null;
-  }
-
-  return attendee;
 };
